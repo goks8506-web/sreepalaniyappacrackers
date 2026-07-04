752c9df315823d63c82c8e01e7f44d697a467f19
GokulKannan3 <gokul8506@gmail.com>
2026-07-04 18:36:59 +0530

    main

--- Config.jsx
+++ Config.jsx
@@ -1,3 +1,3 @@
-export const API_BASE_URL = `https://spc-back.vercel.app`;
+export const API_BASE_URL = `https://spcbackend.sripalaniyappacrackers.com`;
 // export const API_BASE_URL = `https://madhunishabackend.onrender.com`;
 // export const API_BASE_URL = 'http://localhost:5000';
